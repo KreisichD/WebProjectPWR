ba995fc8bf45acff72c0842eb5a0ba5fce9efde7
Damian Kreisich <kreisichd@gmail.com>
2018-11-13 19:40:48 +0100

    Add javascript tests

---
+++ PWRProject/JSCombos.js
@@ -0,0 +1,17 @@
+﻿var element = document.createElement("p");
+
+function alertSomething() {
+    alert('Przykładowy alert z listenerem');
+    var container = document.getElementsByClassName("container");
+
+    var node = document.createTextNode("Click this one to prompt");
+    element.appendChild(node);
+    container[0].appendChild(element);
+    element.addEventListener("click", prompter);
+}
+function prompter() {
+    var name = prompt("Let me know, whats your name", "NONE");
+    document.getElementById("miejsce").innerHTML = 'Witam Cię na stronie :)';
+    element.innerHTML = name;
+}
+document.getElementById("alert").addEventListener("click", alertSomething);
