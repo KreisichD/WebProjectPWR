d7880bffc84870ebf3ba3219e73981bd04b8c3b5
piotrek97 <p.gramacki@gmail.com>
2018-11-19 22:16:59 +0100

    add form to modify style on AboutUs page with JS

---
+++ PWRProject/AboutUsPageSettings.js
@@ -0,0 +1,51 @@
+var fontHelpMessage = "Use only listed fonts pls ;)"
+var helper;
+
+function start() {
+    helper = document.getElementById("helper");
+
+    registerInputListeners(document.getElementById("selectFont"));
+
+    registerConfirmListeners(document.getElementById("pageSettings"));
+}
+
+function registerConfirmListeners(form) {
+    form.addEventListener("submit",
+        function () {
+            var isConfirmed = confirm("Are yue sure want to submit?");
+            if (isConfirmed) {
+                applyChanges(form);
+            }
+        }, false);
+
+    form.addEventListener("reset",
+        function () {
+            alert("Form reset complete");
+        }, false);
+}
+
+function registerInputListeners(object) {
+    object
+
+    object.addEventListener("focus",
+        function () {
+            helper.innerHTML = fontHelpMessage;
+        }, false);
+
+    object.addEventListener("blur",
+        function () {
+            helper.innerHTML = "";
+        }, false);
+}
+
+function applyChanges(form) {
+    var backgroundColor = form.elements["selectBackground"];
+    var fontColor = form.elements["selectFontColor"];
+    var font = form.elements["selectFont"];
+
+    document.body.setAttribute("style", "background-color: " + backgroundColor.value + "; font-family: " + font.value);
+    
+    document.getElementById("toChange").setAttribute("style", "color: " + fontColor.value);
+}
+
+window.addEventListener("load", start, false);
