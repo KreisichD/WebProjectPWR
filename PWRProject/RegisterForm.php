a71eea015c59ad6e95d59a2194b269579a9b15b3
Damian Kreisich <kreisichd@gmail.com>
2018-12-18 13:29:57 +0100

    Add register form, users add to database after successful register

--- PWRProject/RegisterForm.php
+++ PWRProject/RegisterForm.php
@@ -1,11 +1,12 @@
 <?php
-//include_once 'DBH.php';
+include_once 'DBH.php';
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <?php
+	<?php
+	$verification = false;
 	if (!empty($_POST)){
 		$f_name =       $_POST["f_name"];
 		$l_name =       $_POST["l_name"];
@@ -66,29 +67,92 @@
 						$result=$result."Phone should have a nine-digits <br />";
 					}
 					if ($result==""){
-                        $pass = $GLOBALS['pass'];
-						$result="Given data is valid $pass <br />";
+						$pass = $GLOBALS['pass'];
+						$result="Given data is valid, your password is $pass <br />";
+						$GLOBALS['verification'] = true;
 					}
 					return $result;
 	}
-	function iSeeThatYouLike(){
+	function getProgrammingLanguageId(){
+		$conn = $GLOBALS['conn'];
 		$result = "I can see that you know ";
 		if (isset($GLOBALS['langs'])){
-            $langs = $GLOBALS['langs'];
-            foreach ($langs as &$veal){
-                $result = $result.$veal.'|';
-            }
-            $result = $result." that's amazing";
-        }
-        else{
-            $result = $result."nothing";
-        }
-        echo $result."<br />";
+			$langs = $GLOBALS['langs'];
+			$query = 'SELECT LangID FROM programminglanguages';
+			$proglang = 'ProgLanguage = ';
+			$separator = ' WHERE ';
+			foreach ($langs as &$veal){
+				$query = $query.$separator.$proglang.'\''.$veal.'\'';
+				$separator = ' OR ';
+			}
+		}
+		else{
+				return NULL;
+		}    
+		if (!$result = mysqli_query($conn, $query))
+			return NULL;
+		return $result;
+	}
+	function checkUniversity($university){
+		$conn = $GLOBALS['conn'];
+		
+		$query = 'SELECT UniversityId FROM universities WHERE UniversityName = ';
+		$query = $query.'\''.$university.'\'';
+		if (!$result = mysqli_query($conn, $query) or mysqli_num_rows($result)<=0)
+			return -1;
+
+		$i = mysqli_fetch_assoc($result);
+		
+		return $i['UniversityId'];
+			
+	}
+	function prepareDate(){
+		$year = $GLOBALS['date_y'];
+		$month = $GLOBALS['date_m'];
+		$day = $GLOBALS['date_d'];
+		$originalDate = $year.'-'.$month.'-'.$day;
+		$newDate = new DateTime($originalDate);
+		return $newDate->format('y-m-d');
+	}
+    function getUserId($mail){
+        $conn = $GLOBALS['conn'];
+
+        $query = 'SELECT UserID FROM sysusers WHERE E_mail = '.'\''.$mail.'\'';
+        $result = mysqli_query($conn, $query);
+        return mysqli_fetch_assoc($result)['UserID'];
     }
 	function addUserToDatabase(){
+		$conn = $GLOBALS['conn'];
 
+        //checking university
+		$university = $GLOBALS['university'];
+		$uniid = checkUniversity($university);
+		
+		if ($uniid === -1){
+			$query = 'INSERT INTO universities(UniversityId, UniversityName) VALUES (0,'.' \''.$university.'\''.')';
+			mysqli_query($conn, $query);
+			$uniid = checkUniversity($university);
+		}
+        //inserting user
+		$date = prepareDate();
+		$query = 'INSERT INTO sysusers
+		(UserID, E_mail, UsrPass, FirstName, LastName, DateOfBirth, Sex, PhoneNr, University, AboutMe)
+		VALUES (0, '.'\''.$GLOBALS['e_mail'].'\''.','.'\''.$GLOBALS['pass'].'\''.','.'\''.$GLOBALS['f_name'].'\''.'
+		,'.'\''.$GLOBALS['l_name'].'\''.','.'\''.$date.'\''.','.'\''.$GLOBALS['sex'].'\''.','.'\''.$GLOBALS['phone'].'\''.'
+		,'.$uniid.','.'\''.$GLOBALS['about'].'\''.')';
+        mysqli_query($conn, $query);
+        //inserting connections
+        $userid = getUserId($GLOBALS['e_mail']);
+        $result = getProgrammingLanguageId();
+        if ($result != NULL){
+            while($row = mysqli_fetch_row($result)){
+                $query = 'INSERT INTO knownlangs(ProgLangID, UsrID) VALUES (';
+                $query = $query.$row[0].', '.$userid.')';
+                mysqli_query($conn, $query);
+            }
+        }
 	}
-    ?>
+	?>
 
 	<meta charset="utf-8" />
 	<meta name="description" content="Informations about authors" />
@@ -151,7 +215,12 @@ include 'setBackground.php';
 			<div class="off1 col7">
 				<?php		
 				  print(verifyPostedData());
-				  iSeeThatYouLike();
+
+				  if ($GLOBALS['verification']){
+					  addUserToDatabase();
+				  }
+
+				  mysqli_close($GLOBALS['conn']);
 				?>
 			</div>
 			<!--<div class="off1 col7">
